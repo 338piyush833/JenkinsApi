7b011121249bbf32a9b66748f842a1d91bfbe7df
338piyush833 <piyush.sharma@intimetec.com>
2022-03-28 23:27:55 +0530

    report of next day

--- JenkinsApi/Controllers/WeatherForecastController.cs
+++ JenkinsApi/Controllers/WeatherForecastController.cs
@@ -29,7 +29,7 @@ namespace JenkinsApi.Controllers
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateTime.Now.AddDays(index),
+                Date = DateTime.Now.AddDays(index+1),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
